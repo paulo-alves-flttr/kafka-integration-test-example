22850c4b1d587741fc45156a9f17ce4f896f6d1d
paulo-alves-flttr <paulo.alves@flutter.com>
2024-07-23 18:31:23 +0100

    Refactored integration tests to use spring boot docker compose

--- integration-tests/src/test/java/com/example/integrationtests/IntegrationTestsApplicationTests.java
+++ integration-tests/src/test/java/com/example/integrationtests/IntegrationTestsApplicationTests.java
@@ -1,22 +1,19 @@
 package com.example.integrationtests;
 
-import org.junit.ClassRule;
+import com.example.betsapi.BetsApiApplication;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import com.example.betsapi.BetsApiApplication;
+import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.test.context.ActiveProfiles;
-import org.testcontainers.containers.DockerComposeContainer;
-
-import java.io.File;
 
 @SpringBootTest(classes = BetsApiApplication.class, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @ActiveProfiles("test")
 class IntegrationTestsApplicationTests {
-    @ClassRule
-    public static DockerComposeContainer environment = new DockerComposeContainer(
-            new File("src/test/resources/docker/compose.yaml"));
+    @LocalServerPort
+    private int port;
 
     @Test
     void contextLoads() {
+        System.out.println("Hello World!");
     }
 }
